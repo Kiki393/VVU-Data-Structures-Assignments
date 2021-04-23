2b7aa04e7b419db4966e1adb0356abffe01a632c
Victor Ashun <ashun393@gmail.com>
2021-04-23 06:58:52 +0000

    Doubly

---
+++ DoublyLinkedList/src/main/java/com/mycompany/doublylinkedlist/NewMain.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.doublylinkedlist;
+
+/**
+ *
+ * @author Kiki Ashun
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    /* Given a node as prev_node, insert a new node after the given node */
+    //public void InsertAfter(Node prev_Node, String new_data) {
+
+        /*1. check if the given prev_node is NULL */
+        //if (prev_Node == null) {
+           // System.out.println("The given previous node cannot be NULL ");
+            //return;
+        // }
+
+        /* 2. allocate node 
+    * 3. put in the data */
+        //Node new_node = new Node(new_data);
+
+        /* 4. Make next of new node as next of prev_node */
+        //new_node.next = prev_Node.next;
+
+        /* 5. Make the next of prev_node as new_node */
+        //prev_Node.next = new_node;
+
+        /* 6. Make prev_node as previous of new_node */
+        //new_node.prev = prev_Node;
+
+        /* 7. Change previous of new_node's next node */
+       // if (new_node.next != null) {
+          //  new_node.next.prev = new_node;
+       // }
+   // }
+    
+    // Insert 8, after 7. So linked 
+        // list becomes 1->7->8->6->4->NULL
+        //dll.InsertAfter(dll.head.next, 8);
+    
+    //Print list method
+        
+}
